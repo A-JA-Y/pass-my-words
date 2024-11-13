06ea03a23a3492818b756f074f987512001610f5
Ajay <mailtoajay7007@gmail.com>
2024-11-13 22:16:46 +0530

    Project started + tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return <>
+    <div className=" w-full h-screen bg-zinc-900  text-white text-center ">
+      <h1 className=" text-4xl py-3 font-thin drop-shadow-lg ">&lt;Pass My Words/&gt;</h1>
+    </div>
+  </>;
+}
+
+export default App;
